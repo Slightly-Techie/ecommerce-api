32c6b1a0d5be455ae834945f44369ec917a1596e
JerryAgbesi <jerryeagbesi@gmail.com>
2023-07-01 10:30:29 +0000

    Add UserMailer class

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,10 +1,2 @@
-# app/mailers/user_mailer.rb
 class UserMailer < ApplicationMailer
-    default_url_options[:host] = 'http://127.0.0.1:8080/'  # Replace with your actual host and port
-  
-    def confirmation_email(user)
-      @user = user
-      mail(to: @user.email, subject: 'Confirm Your Email')
-    end
-  end
-  
+end
