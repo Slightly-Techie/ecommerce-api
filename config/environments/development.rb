a91d9a0acf6994f9b31948c8d3ef1677f47ad5ad
Essilfie <bquansah007@gmail.com>
2023-07-04 11:25:39 +0000

    Fix stmp config error

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -65,13 +65,14 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.default_url_options = { host: "localhost", port: 3000 }
 
-  #SMTP settings for gmail
+  # SMTP settings for gmail
   config.action_mailer.smtp_settings = {
-    :address              => "smtp.gmail.com",
-    :port                 => 587,
-    :user_name            => ENV["GMAIL_USERNAME"],
-    :password             => ENV["GMAIL_APP_PASSWORD"],
-    :authentication       => "plain",
-    :enable_starttls_auto => true
-  }
+  address: "smtp.gmail.com",
+  port: 587,
+  domain: "gmail.com",
+  user_name: ENV.fetch("GMAIL_USERNAME", nil),
+  password: ENV.fetch("GMAIL_APP_PASSWORD", nil),
+  authentication: "plain",
+  enable_starttls_auto: true
+}
 end
