bb50c3c40064282f2f5df19f3c7a6287a81b7d5f
greatnessmensah <ephraimmensah99@gmail.com>
2023-07-04 21:42:40 +0000

    implemented user sugn ups

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -1,5 +1,8 @@
 module Types
+<<<<<<< HEAD
   class MutationType < Types::BaseObject
+=======
+>>>>>>> fd74ba9 (implemented user sugn ups)
     field :create_user, mutation: Mutations::CreateUser
     field :confirm_email, mutation: Mutations::ConfirmEmail
   end
