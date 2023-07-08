c455714ec9221ab1de738b2e40705d7311459a99
JerryAgbesi <jerryeagbesi@gmail.com>
2023-07-08 20:51:32 +0000

    Add users_controller

--- app/graphql/mutations/create_user.rb
+++ app/graphql/mutations/create_user.rb
@@ -7,25 +7,14 @@ module Mutations
     argument :password, String, required: true
 
     field :user, Types::UserType, null: true
-<<<<<<< HEAD
-    field :errors, [String], null: true
-
-    def resolve(username:, email:, last_name:, other_names:, password:)
-=======
     field :errors, [String], null: false
 
     def resolve(username:, email:, password:)
->>>>>>> fd74ba9 (implemented user sugn ups)
       password_digest = BCrypt::Password.create(password)
 
       user = User.new(
         username: username,
         email: email,
-<<<<<<< HEAD
-        other_names: other_names,
-        last_name: last_name,
-=======
->>>>>>> fd74ba9 (implemented user sugn ups)
         password_digest: password_digest,
         active: true,
         email_confirmed: false,
@@ -33,15 +22,9 @@ module Mutations
       )
 
       if user.save
-<<<<<<< HEAD
-        respond 201, user: user, error: []
-      else
-        respond 400, user: nil, error: user.errors
-=======
         { user: user, errors: [] }
       else
         { user: nil, errors: user.errors.full_messages }
->>>>>>> fd74ba9 (implemented user sugn ups)
       end
     end
   end
--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -4,8 +4,6 @@ class UserMailer < ApplicationMailer
     mail(to: @user.email, subject: "Welcome to the ST Market")
   end
 
-  default_url_options[:host] = 'http://127.0.0.1:8080/'  # Replace with your actual host and port
-  
   def confirmation_email(user)
     @user = user
     mail(to: @user.email, subject: 'Confirm Your Email')
