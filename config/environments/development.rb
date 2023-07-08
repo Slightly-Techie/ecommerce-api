6e79d47784213d36b0b62252729dc76853bface6
JerryAgbesi <jerryeagbesi@gmail.com>
2023-07-08 18:19:32 +0000

    Update environment files

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -70,7 +70,7 @@ Rails.application.configure do
     :address              => "smtp.gmail.com",
     :port                 => 587,
     :user_name            => ENV["GMAIL_USERNAME"],
-    :password             => ENV["GMAIL_APP_PASSWORD"]
+    :password             => ENV["GMAIL_APP_PASSWORD"],
     :authentication       => "plain",
     :enable_starttls_auto => true
   }
