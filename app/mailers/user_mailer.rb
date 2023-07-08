dfec1e50bb186e99f9672dde571585a1cf52fa79
JerryAgbesi <jerryeagbesi@gmail.com>
2023-07-08 18:19:04 +0000

    Update user mailer for password reset

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -12,6 +12,13 @@ class UserMailer < ApplicationMailer
     mail(to: @user.email, subject: 'Confirm Your Email')
   end
 
+  default_url_options[:host] = 'http://127.0.0.1:8080/'  # Replace with your actual host and port
+  
+  def confirmation_email(user)
+    @user = user
+    mail(to: @user.email, subject: 'Confirm Your Email')
+  end
+
   def forgot_password_mail(user)
     @user = user
     mail(to: @user.email, subject: "Reset Your Password")
