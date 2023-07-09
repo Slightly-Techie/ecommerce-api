8132d1cb2315abc92bba763d754dd60d72908072
Essilfie <bquansah007@gmail.com>
2023-07-09 16:35:00 +0000

    Update SMTP configuration

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -62,17 +62,5 @@ Rails.application.configure do
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
 
-  config.action_mailer.delivery_method = :smtp
   config.action_mailer.default_url_options = { host: "localhost", port: 3000 }
-
-  # SMTP settings for gmail
-  config.action_mailer.smtp_settings = {
-  address: "smtp.gmail.com",
-  port: 587,
-  domain: "gmail.com",
-  user_name: ENV.fetch("GMAIL_USERNAME", nil),
-  password: ENV.fetch("GMAIL_APP_PASSWORD", nil),
-  authentication: "plain",
-  enable_starttls_auto: true
-}
 end
