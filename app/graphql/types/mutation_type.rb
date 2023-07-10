67cf3aac64623809d8f0ef241062157b3dbb7437
JerryAgbesi <jerryeagbesi@gmail.com>
2023-07-10 12:52:44 +0000

    Add password reset  mutaton to mutations types

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -2,5 +2,6 @@ module Types
   class MutationType < Types::BaseObject
     field :create_user, mutation: Mutations::CreateUser
     field :confirm_email, mutation: Mutations::ConfirmEmail
+    field :password_reset_email, mutation: Mutations::PasswordResetEmail
   end  
 end
