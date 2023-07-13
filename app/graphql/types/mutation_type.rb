5eebdcbb88adaecf3edb0d281edc7e7bf7f10cc1
JerryAgbesi <jerryeagbesi@gmail.com>
2023-07-13 10:15:48 +0000

    Add mutation for resending confirmation email

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -3,5 +3,6 @@ module Types
     field :create_user, mutation: Mutations::CreateUser
     field :confirm_email, mutation: Mutations::ConfirmEmail
     field :password_reset_email, mutation: Mutations::PasswordResetEmail
+    field :resend_confirmation_email, mutation: Mutations::ResendConfirmationEmail
   end  
 end
