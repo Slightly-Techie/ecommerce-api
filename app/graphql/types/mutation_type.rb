9226b63fd43fabb9e447ca8e5021df51e6dd860a
kwame bio <kwameagyemang73@gmail.com>
2023-07-16 19:04:27 +0000

    Code for User Sessions

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -4,6 +4,7 @@ module Types
     field :confirm_email, mutation: Mutations::ConfirmEmail
     field :user_login, mutation: Mutations::UserLogin
     field :user_logout, mutation: Mutations::UserLogout
+    field :password_reset_email, mutation: Mutations::PasswordResetEmail
     field :resend_confirmation_email, mutation: Mutations::ResendConfirmationEmail
   end
 end
