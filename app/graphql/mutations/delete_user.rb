ba3f515be5c96fb5e5b898b58f1c9ed8a7234228
kwame bio <kwameagyemang73@gmail.com>
2023-09-05 16:26:46 +0000

    user authorization to update and delete user profile

---
+++ app/graphql/mutations/delete_user.rb
@@ -0,0 +1,19 @@
+class DeleteUser < BaseMutation
+    argument :email, String, required: true
+    field :user, Types::UserType, null: false
+
+    def resolve(email:, password:, context:)
+        user = context[:current_user]
+        return { user: nil, errors: ["Authentication required"] } unless current_user
+        
+        user.find_by(email: email)
+        return { user: nil, errors: ["Unauthorized to update profile"] } unless user == current_user
+
+        if user.delete(user: user)
+            { user: user, errors: [] }
+        else
+            { user: nil, errors: user.errors.full_messages }
+        end
+    end
+
+end
