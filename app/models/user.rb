4d0d4240f24ed8774ed1a5214cd98efc7e28221b
Essilfie <bquansah007@gmail.com>
2023-09-15 23:06:06 +0000

    Clean up

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,44 +1,43 @@
-# == Schema Information
-#
-# Table name: users
-#
-#  id                   :bigint           not null, primary key
-#  active               :boolean          default(FALSE)
-#  confirmation_token   :string
-#  email                :string           not null
-#  email_confirmed      :boolean          default(FALSE)
-#  first_name           :string           not null
-#  last_name            :string           not null
-#  password_digest      :string           not null
-#  password_reset_token :string
-#  points               :decimal(, )      default(0.0)
-#  username             :string           not null
-#  created_at           :datetime         not null
-#  updated_at           :datetime         not null
-#
-# Indexes
-#
-#  index_users_on_email     (email)
-#  index_users_on_username  (username)
-#
-class User < ApplicationRecord
-  has_secure_password
+ # == Schema Information
+ #
+ Table name: users
+ #
+ #  id                   :bigint           not null, primary key
+ #  active               :boolean          default(FALSE)
+ #  confirmation_token   :string
+ #  email                :string           not null
+ #  email_confirmed      :boolean          default(FALSE)
+ #  first_name           :string           not null
+ #  last_name            :string           not null
+ #  password_digest      :string           not null
+ #  password_reset_token :string
+ #  points               :decimal(, )      default(0.0)
+ #  username             :string           not null
+ #  created_at           :datetime         not null
+ #  updated_at           :datetime         not null
+ #
+ # Indexes
+ #
+ #  index_users_on_email     (email)
+ #  index_users_on_username  (username)
+ #
+ class User < ApplicationRecord
+   has_secure_password
 
-  validates :username, uniqueness: true, presence: true
-  validates :first_name, :last_name, presence: true
-  validates_with EmailAddress::ActiveRecordValidator, field: :email
+   validates :username, uniqueness: true, presence: true
+   validates :first_name, :last_name, presence: true
+   validates_with EmailAddress::ActiveRecordValidator, field: :email
 
-  after_create :send_confirmation_email
+   after_create :send_confirmation_email
 
-  def send_confirmation_email
-    update(confirmation_token: rand(000000..999999))
-    UserMailer.welcome_mail(self).deliver_now
-  end
+   def send_confirmation_email
+     update(confirmation_token: rand(000000..999999))
+     UserMailer.welcome_mail(self).deliver_now
+   end
 
-  def send_password_reset_email
-    update!(password_reset_token: rand(000000..999999))
+   def send_password_reset_email
+     update!(password_reset_token: rand(000000..999999))
 
-    UserMailer.forgot_password_mail(self).deliver_now
-  end
-end
-
+     UserMailer.forgot_password_mail(self).deliver_now
+   end
+ end
