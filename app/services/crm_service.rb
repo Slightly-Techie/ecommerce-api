701bfff1f09bbd0e870fec2a438028dfc036aec9
Essilfie <bquansah007@gmail.com>
2023-09-18 13:45:13 +0000

    update base uri for CRM service

--- app/services/crm_service.rb
+++ app/services/crm_service.rb
@@ -1,9 +1,9 @@
 class CrmService
   include HTTParty
-  base_uri "https://crm-api.fly.dev"
+  base_uri "https://crm-api.fly.dev/api/v1"
 
   def self.fetch_user_info(email)
-    response = get("/user_info", query: { email: email }).parsed_response
+    response = get("/users/user_info", query: { email: email }).parsed_response
     response["status"] == 200 ? response["data"] : nil
   end
 end
