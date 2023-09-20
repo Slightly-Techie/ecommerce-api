bb0313d2d79e7efdc32240d7ed48acbd1bd9ca74
Essilfie <bquansah007@gmail.com>
2023-09-20 10:08:13 +0000

    Update mail

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: "Kwame from Slightly Techie"
+  default from: email_address_with_name("shop@slightlytechie.com", "Kwame from Slightly Techie")
   layout "mailer"
 end
