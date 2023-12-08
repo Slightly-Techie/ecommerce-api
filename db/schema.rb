204d1e917cc942769661e0fc5cb8b3cf2ea30b39
kwame bio <kwameagyemang73@gmail.com>
2023-12-08 11:28:52 +0000

    remove add_token_expiration_date migration file

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_12_05_000914) do
+ActiveRecord::Schema[7.0].define(version: 2023_12_03_171306) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -191,23 +191,23 @@ ActiveRecord::Schema[7.0].define(version: 2023_12_05_000914) do
   end
 
   create_table "users", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
     t.string "username", null: false
     t.string "email", null: false
+    t.integer "account_type", default: 0
     t.string "password_digest", null: false
+    t.string "phone_number"
+    t.jsonb "social_links"
     t.boolean "active", default: false
-    t.decimal "points", default: "0.0"
     t.boolean "email_confirmed", default: false
     t.string "password_reset_token"
+    t.datetime "token_expiration_date", precision: nil
     t.string "confirmation_token"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "last_name"
-    t.string "other_names"
-    t.datetime "token_expiration_date"
-    t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["password_reset_token"], name: "index_users_on_password_reset_token", unique: true
-    t.index ["username"], name: "index_users_on_username", unique: true
+    t.index ["email"], name: "index_users_on_email"
+    t.index ["username"], name: "index_users_on_username"
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
